f73fb3af729a1be86d86d85e0c3209182852896f
kurban15 <kyleurban99@gmail.com>
2025-03-31 17:40:10 -0700

    fixed default time zone to LA and initialized modal

--- main.js
+++ main.js
@@ -3,18 +3,40 @@ const dayjs = require ('dayjs');
 const utc = require("dayjs/plugin/utc");
 const timezone = require("dayjs/plugin/timezone");
 
+const MicroModal = require('micromodal');
+document.addEventListener("DOMContentLoaded", function () {
+    MicroModal.init({
+        disableScroll: true,
+        awaitOpenAnimation: true, 
+        awaitCloseAnimation: true
+    });
+
+    const icon = document.getElementById('icon');
+    icon.addEventListener('click', function() {
+        MicroModal.show('modal-1');
+    });
+
+    const closeButton = document.querySelector('[data-micromodal-close]');
+    closeButton.addEventListener('click', function() {
+        MicroModal.close('modal-1');
+    });
+});
+
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
 setInterval(updateTime, 1000)
 
 function updateTime() {
-    var timezone = dayjs.tz.setDefault("America/Los_Angeles");
+    const defaultTimezone = "America/Los_Angeles";
+    var timezone = defaultTimezone.replace(/_/g, " ");
     document.querySelector('.timezone').innerText = timezone;
 
-    const now = dayjs().format('h:mm:ss');
+    const now = dayjs().format('hh:mm:ss');
     document.querySelector('.time').innerText = now;
 
     var date = dayjs().format('dddd, MMMM D, YYYY');
     document.querySelector('.date').innerText = date;
 }
+
+//browserify main.js -o bundle.js
